3a0d62d4f1c65eae0ff1bdd82c045dc4c162dbed
Rafael Ferreira  Melo <101290871+Rafael-FerreiraM@users.noreply.github.com>
2024-01-08 19:11:27 -0300

    Update deletar.php

--- painelcms/painel_admin/deletar.php
+++ painelcms/painel_admin/deletar.php
@@ -1,6 +1,6 @@
 <?php
 	if(isset($_POST['id_membro'])){
-	 $pdo = new PDO('mysql:host=localhost;dbname=bootstrap_projeto','root','');
+	 $pdo = new PDO('mysql:host=localhost;dbname=painelcms','root','');
 	 $sql = $pdo->prepare("DELETE FROM `tb_equipe` WHERE id = ?");
 	 $sql->execute(array($_POST['id_membro']));
 	}
